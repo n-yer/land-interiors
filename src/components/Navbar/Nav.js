b05000832062ef769d8b2b01e3a2887755dac201
n-yer <60143911+n-yer@users.noreply.github.com>
2024-04-03 10:22:58 +0200

    responsiveness

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -8,7 +8,7 @@ export default function Nav({align, openMenu, handleCloseMenu, handleOpenMenu}) 
     <nav className={'navbar'+(align==='right'?' lg':'')}>
       <ul className={"nav-links"+(openMenu?" nav-expanded":"")} style={{justifyContent:align==='right'?'flex-end':'center'}}>
         {navLinks}
-        <div className='close-menu-btn-container'style={{display:'flex', width:'100%', justifyContent:'center'}}>
+        <div className='close-menu-btn-container'>
 
         <button className='cta-btn close-menu-btn' style={{width:'20px'}} onClick={handleCloseMenu}>Close</button>
         </div>
